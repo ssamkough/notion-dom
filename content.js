c219a31092997b0d1bf3c26c38552aae38d04f40
Sammy Samkough <sammysamkough@gmail.com>
2020-05-13 05:04:11 -0400

    Added the ability to open the popup, check the radio buttons, and prelim functions before removing an element

--- content.js
+++ content.js
@@ -1,15 +1,16 @@
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function initializeElements() {
+  let htmlElement = document.documentElement;
   let bodyElement = document.getElementsByTagName("body")[0];
 
   let mouseoverNode = document.createElement("div");
-  mouseoverNode.setAttribute("id", "mouseover_overlay");
+  mouseoverNode.setAttribute("id", "mouseover-overlay");
 
   bodyElement.appendChild(mouseoverNode);
 
   mouseoverNode.style.position = "fixed";
-  mouseoverNode.style.zIndex = 999999999999999;
+  mouseoverNode.style.zIndex = 7000;
   mouseoverNode.style.left = 0;
   mouseoverNode.style.top = 0;
   mouseoverNode.style.width = 0;
@@ -19,33 +20,93 @@ function initializeElements() {
   mouseoverNode.style.transition = "0.2s";
 
   let popupOverlay = document.createElement("div");
-  let popupContent = document.createElement("div");
+  let popupContainer = document.createElement("div");
+  let popupHeader = document.createElement("div");
+  let popupBody = document.createElement("div");
+  let popupFooter = document.createElement("div");
   let popupTitle = document.createElement("h2");
-  let popupText = document.createElement("p");
+  let popupBodyRadioGroups = document.createElement("form");
+  let popupBodyRemoveElementRadioBox = document.createElement("div");
+  let popupBodyRemoveElementRadio = document.createElement("input");
+  let popupBodyRemoveElementText = document.createElement("span");
+  let popupBtnX = document.createElement("button");
   let popupBtnClose = document.createElement("button");
+  let popupBtnSubmit = document.createElement("button");
 
   popupOverlay.setAttribute("class", "popup-overlay");
-  popupContent.setAttribute("class", "popup-content");
-  popupBtnClose.setAttribute("class", "btn-close");
-  popupTitle.innerHTML = "Pop-Up";
-  popupText.innerHTML = "This is an example pop-up.";
-  popupBtnClose.innerHTML = "Close";
+  popupContainer.setAttribute("class", "popup-container");
+  popupHeader.setAttribute("class", "popup-header");
+  popupBody.setAttribute("class", "popup-body");
+  popupBodyRadioGroups.setAttribute("class", "popup-body-radio-groups");
+  popupBodyRemoveElementRadioBox.setAttribute(
+    "class",
+    "popup-body-remove-element-radio-box"
+  );
+  popupBodyRemoveElementRadio.setAttribute("type", "radio");
+  popupBodyRemoveElementRadio.setAttribute("name", "remove-element");
+  popupBodyRemoveElementRadio.setAttribute("value", "remove element");
+  popupBodyRemoveElementRadio.setAttribute(
+    "class",
+    "popup-body-radio popup-body-remove-element-radio"
+  );
+  popupFooter.setAttribute("class", "popup-footer");
+  popupBtnX.setAttribute("class", "popup-close-btn");
+  popupBtnClose.setAttribute("class", "popup-btn popup-close-btn");
+  popupBtnSubmit.setAttribute("class", "popup-btn popup-submit-btn");
 
-  bodyElement.appendChild(popupOverlay);
-  popupOverlay.appendChild(popupContent);
-  popupContent.appendChild(popupTitle);
-  popupContent.appendChild(popupText);
-  popupContent.appendChild(popupBtnClose);
+  htmlElement.appendChild(popupOverlay);
+  popupOverlay.appendChild(popupContainer);
+  popupContainer.appendChild(popupHeader);
+  popupContainer.appendChild(popupBody);
+  popupContainer.appendChild(popupFooter);
+  popupHeader.appendChild(popupTitle);
+  popupHeader.appendChild(popupBtnX);
+  popupBody.appendChild(popupBodyRadioGroups);
+  popupBodyRadioGroups.appendChild(popupBodyRemoveElementRadioBox);
+  popupBodyRemoveElementRadioBox.appendChild(popupBodyRemoveElementRadio);
+  popupBodyRemoveElementRadioBox.appendChild(popupBodyRemoveElementText);
+  popupFooter.appendChild(popupBtnClose);
+  popupFooter.appendChild(popupBtnSubmit);
+
+  popupTitle.innerHTML = "What would you like to do with this element?";
+  popupBodyRemoveElementText.textContent = " Remove Element";
+  popupBtnX.innerHTML = "&times;";
+  popupBtnClose.innerHTML = "Cancel";
+  popupBtnSubmit.innerHTML = "Submit";
 
   popupOverlay.style.visibility = "hidden";
   popupOverlay.style.position = "absolute";
   popupOverlay.style.background = "#FFFFFF";
-  popupOverlay.style.border = "3px solid #666666";
-  popupOverlay.style.width = "50%";
-  popupOverlay.style.height = "50%";
+  popupOverlay.style.width = "50vw";
+  popupOverlay.style.height = "50vh";
+  popupOverlay.style.top = "25%";
   popupOverlay.style.left = "25%";
+  popupOverlay.style.zIndex = 9000;
 
-  popupContent.style.visibility = "hidden";
+  popupContainer.style.display = "flex";
+  popupContainer.style.flexDirection = "column";
+  popupContainer.style.justifyContent = "space-between";
+  popupContainer.style.height = "100%";
+
+  popupHeader.style.margin = "10px";
+
+  popupTitle.style.float = "left";
+
+  popupBtnX.style.fontSize = "2em";
+  popupBtnX.style.display = "inline-block";
+  popupBtnX.style.verticalAlign = "middle";
+  popupBtnX.style.borderRadius = "0";
+  popupBtnX.style.margin = ".20rem";
+  popupBtnX.style.color = "#666666";
+  popupBtnX.style.background = "#FFFFFF";
+  popupBtnX.style.border = "0";
+  popupBtnX.style.float = "right";
+
+  popupHeader.style.margin = "10px";
+
+  popupBody.style.padding = "10px";
+
+  popupFooter.style.padding = "10px";
 
   popupBtnClose.style.display = "inline-block";
   popupBtnClose.style.verticalAlign = "middle";
@@ -55,6 +116,17 @@ function initializeElements() {
   popupBtnClose.style.color = "#666666";
   popupBtnClose.style.background = "#FFFFFF";
   popupBtnClose.style.border = "1px solid #666666";
+  popupBtnClose.style.float = "right";
+
+  popupBtnSubmit.style.display = "inline-block";
+  popupBtnSubmit.style.verticalAlign = "middle";
+  popupBtnSubmit.style.borderRadius = "30px";
+  popupBtnSubmit.style.margin = ".20rem";
+  popupBtnSubmit.style.fontSize = "1rem";
+  popupBtnSubmit.style.color = "#666666";
+  popupBtnSubmit.style.background = "#FFFFFF";
+  popupBtnSubmit.style.border = "1px solid #666666";
+  popupBtnSubmit.style.float = "right";
 }
 
 function runRules() {
@@ -64,31 +136,65 @@ function runRules() {
 initializeElements();
 runRules();
 
+// element manipulation option functions
+function removeElementDisplay(element) {
+  console.log("remove element");
+  if (element !== undefined) {
+    element.style.display = "none";
+  }
+}
+
+function addElementDisplay(element) {
+  if (element !== undefined) {
+    element.style.display = "block";
+  }
+}
+
+function removeElement(element) {
+  /* checks to see if popup checkbox is checked or not */
+}
+
+// modal functions
+function closeModal() {
+  // console.log("close");
+  let bodyElement = document.getElementsByTagName("body")[0];
+  bodyElement.style.backgroundColor = "transparent";
+  bodyElement.style.opacity = "1";
+
+  let popupOverlayElement = document.getElementsByClassName("popup-overlay")[0];
+  popupOverlayElement.style.visibility = "hidden";
+}
+
+function processModal(targetElement) {
+  // console.log(targetElement);
+  let radioBtns = document.getElementsByClassName("popup-body-radio");
+  for (let i = 0; i < radioBtns.length; i++) {
+    if (radioBtns[i].checked) {
+      if (radioBtns[i].value == "remove-element") {
+        removeElementDisplay(targetElement);
+      }
+    }
+  }
+}
+
 function clickedElement(element, overlay) {
   overlay.style.top = "0px";
   overlay.style.left = "0px";
   overlay.style.width = "0px";
   overlay.style.height = "0px";
 
-  console.log(element.target);
-  let htmlElement = document.documentElement;
-  htmlElement.style.backgroundColor = "black";
-  htmlElement.style.opacity = "0.5";
-  htmlElement.style.zIndex = "9999";
+  let bodyElement = document.getElementsByTagName("body")[0];
+  bodyElement.style.backgroundColor = "black";
+  bodyElement.style.opacity = "0.5";
+  bodyElement.style.zIndex = "8000";
 
   let popupOverlayElement = document.getElementsByClassName("popup-overlay")[0];
-  let popupContentElement = document.getElementsByClassName("popup-content")[0];
-
   popupOverlayElement.style.visibility = "visible";
-  popupContentElement.style.visibility = "visible";
 }
 
 function gotMessage(message, sender, sendResponse) {
-  console.log("gotMessage");
-  console.log(message.txt);
-
   if (message.txt === "add_feature") {
-    let overlay = document.querySelector("#mouseover_overlay");
+    let overlay = document.querySelector("#mouseover-overlay");
     document.addEventListener("mouseover", (e) => {
       let elem = e.target;
       let rect = elem.getBoundingClientRect();
@@ -102,22 +208,30 @@ function gotMessage(message, sender, sendResponse) {
       // for (let i = 0; i < aElements.length; i++) {
       //   aElements[i].style.pointerEvents = "none";
       // }
-
-      document.addEventListener("click", (e) => {
-        document.removeEventListener("mouseover", clickedElement(e, overlay));
-      });
     });
-  }
 
-  if (message.txt === "record") {
-    div = document.getElementsByClassName(
-      "notion-scroller vertical horizontal"
-    )[0].children;
+    // click on an element
+    document.addEventListener("click", (e) => {
+      if (document.removeEventListener) {
+        document.removeEventListener(
+          "mouseover",
+          clickedElement(e.target, overlay)
+        );
 
-    if (div !== undefined) {
-      div[1].style.display = "none";
+        let submitBtn = document.getElementsByClassName("popup-submit-btn")[0];
+        submitBtn.addEventListener("click", processModal(e.target));
+      } else if (document.detachEvent) {
+        document.detachEvent("mouseover", clickedElement(e.target, overlay));
 
-      div[2].style.display = "none";
+        let submitBtn = document.getElementsByClassName("popup-submit-btn")[0];
+        submitBtn.addEventListener("click", processModal(e.target));
+      }
+    });
+
+    let popupCloseBtns = document.getElementsByClassName("popup-close-btn");
+    for (let i = 0; i < popupCloseBtns.length; i++) {
+      // console.log(popupCloseBtns[i]);
+      popupCloseBtns[i].addEventListener("click", closeModal());
     }
   }
 }
